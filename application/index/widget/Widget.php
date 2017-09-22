c02164e092c3391b31d2a418a2c9b8aadad5cbee
dhjkaSTTYJKIYUTSE <769946123@qq.com>
2017-09-22 15:45:03 +0800

    gxq

--- application/index/widget/Widget.php
+++ application/index/widget/Widget.php
@@ -1,23 +1,6 @@
 <?php
 /**
  * Created by PhpStorm.
-<<<<<<< HEAD
- * User: lenovo
- * Date: 2017/9/22
- * Time: 14:11
- */
-namespace app\index\widget;
-use think\Controller;
-
-class Widget extends Controller{
-    public function header(){
-        return $this->fetch('Widget/header');
-    }
-    public function footer(){
-        return $this->fetch('Widget/footer');
-    }
-}
-=======
  * User: asus
  * Date: 2017/9/22
  * Time: 10:16
@@ -34,4 +17,4 @@ class Widget extends Controller{
         return $this->fetch('widget/footer');
     }
 }
->>>>>>> b6409ebe7a7a59ef451c2ab8e2bfa4398dacca54
+
