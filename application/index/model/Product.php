55b319616dd790566a673391f8796621ba53b81a
dhjkaSTTYJKIYUTSE <769946123@qq.com>
2017-09-26 09:57:21 +0800

    gxq

---
+++ application/index/model/Product.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: asus
+ * Date: 2017/9/24
+ * Time: 20:32
+ */
+namespace app\index\model;
+use think\Model;
+class Product extends Model{
+    static public function search($goodsId){
+        $data=db('goods')
+            ->alias('g')
+            ->join('image i', 'g.goods_id=i.goods_id', 'left')
+            ->order('i.is_face')
+            ->where(['g.goods_id'=>$goodsId])
+            ->select();
+        return $data;
+    }
+    static public function foundImg($imgId){
+        $data=db('image')->find($imgId);
+        return $data;
+    }
+
+}
